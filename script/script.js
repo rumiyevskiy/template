70052cfd467f6eeed75c4e4076850933532bd9e0
rumiyevskiy <rumiyevskiy@gmail.com>
2024-08-22 12:23:51 +0300

    dev toggle theme

--- script/script.js
+++ script/script.js
@@ -134,3 +134,79 @@ if(menuLinks.length > 0){
     };
   };
 };
+
+// перемикання кольорових схем
+
+const darkSchemeMedia = matchMedia('(prefers-color-scheme: dark)');
+const switcherRadios = document.querySelectorAll('.switcher__radio');
+
+function setupSwitcher() {
+  const savedScheme = getSavedScheme();
+
+  if (savedScheme !== null) {
+    const currentRadio = document.querySelector(`.switcher__radio[value=${savedScheme}]`);
+    currentRadio.checked = true;
+  };
+
+  [...switcherRadios].forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      setScheme(event.target.value);
+    });
+  });
+ };
+
+function setupScheme() {
+  const savedScheme = getSavedScheme();
+  const systemScheme = getSystemScheme();
+
+  if (savedScheme === null) return;
+
+  if (savedScheme !== systemScheme) {
+    setScheme(savedScheme);
+  };
+};
+
+function setScheme(scheme) {
+  switchMedia(scheme);
+
+  if (scheme === 'auto') {
+    clearScheme();
+  } else {
+    saveScheme(scheme);
+  };
+};
+
+function switchMedia(scheme) {
+
+  if (scheme ==='auto') {
+    document.body.classList.remove('light', 'dark');
+    const bodyClass = darkSchemeMedia.matches ? 'dark' : 'light';
+    document.body.classList.add(bodyClass);
+  } else {
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(scheme);
+  } 
+  
+};
+
+function getSystemScheme() {
+  const darkScheme = darkSchemeMedia.matches;
+
+  return darkScheme ? 'dark' : 'light';
+};
+
+function getSavedScheme() {
+  return localStorage.getItem('color-scheme');
+};
+
+function saveScheme(scheme) {
+  return localStorage.setItem('color-scheme', scheme);
+};
+
+function clearScheme() {
+  return localStorage.removeItem('color-scheme');
+};
+
+setupSwitcher();
+setupScheme();
+
